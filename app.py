dc38811f496ca91e71fc2b1d4461782d9516d042
Steven Lanciotti <steven.lanciotti@berkeley.edu>
2025-10-28 12:40:32 -0400

    update

--- app.py
+++ app.py
@@ -1,5 +1,32 @@
 import streamlit as st
+import altair as alt
+import pandas as pd
+from numpy.random import default_rng as rng
 
 st.title("Week 10 Assignment - Animation")
 
 st.header("This weeks assignment is take any of your previous submissions and add an animation that is focused on a specific insight.", divider="gray")
+
+
+
+df = pd.DataFrame(rng(0).standard_normal((20, 3)), columns=["a", "b", "c"])
+
+point_selector = alt.selection_point("point_selection")
+interval_selector = alt.selection_interval("interval_selection")
+chart = (
+    alt.Chart(df)
+    .mark_circle()
+    .encode(
+        x="a",
+        y="b",
+        size="c",
+        color="c",
+        tooltip=["a", "b", "c"],
+        fillOpacity=alt.condition(point_selector, alt.value(1), alt.value(0.3)),
+    )
+    .add_params(point_selector, interval_selector)
+)
+
+event = st.altair_chart(chart, key="alt_chart", on_select="rerun")
+
+event
