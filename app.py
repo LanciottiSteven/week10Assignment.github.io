3985c41d0bfc8c7c65d5ebb56c15d72bfa810bae
Steven Lanciotti <steven.lanciotti@berkeley.edu>
2025-10-28 12:35:04 -0400

    update

--- app.py
+++ app.py
@@ -1,2 +1,5 @@
 import streamlit as st
-st.write("Hello World")
+
+st.title("Week 10 Assignment - Animation")
+
+st.header("This weeks assignment is take any of your previous submissions and add an animation that is focused on a specific insight.", divider="gray")
